0728028fbed11d93c2fae516066008b3c74b4bfc
Laurent <laurent@tacticaltech.org>
2019-01-13 14:54:39 +0100

    small change in output formatting

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import { calculateScore } from 'lib'
 // Get the results from the command line as an array of arrays
 const results = JSON.parse("[" + process.argv[2] + "]")
 
-console.log('You have entered the following results' + results)
+console.log('You have entered the following results: ' + results)
 
 calculateScore(results)
