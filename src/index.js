3aba1f5b44ae881a32773c18065df3d8146634f6
Laurent <laurent@tacticaltech.org>
2019-01-14 10:28:22 +0100

    adding comments

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
-import _ from 'lodash'
-
 import { calculateScore } from 'lib'
 
 // Get the results from the command line as an array of arrays
-const results = JSON.parse("[" + process.argv[2] + "]")
-
+const results = JSON.parse('[' + process.argv[2] + ']')
 console.log('You have entered the following results: ' + results)
 
+// Calculate and print the score of a bowling game
 calculateScore(results)
+
+export default calculateScore
--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,10 @@
 
 import _ from 'lodash'
 
+/** Calculate the score of a bowling game based on array with frames
+ * @param {array} results - array of frame arrays
+ * @returns {number} score
+ */
 export const calculateScore = results => {
   const frames = validateResults(results)
   let score = 0
@@ -21,6 +25,11 @@ export const calculateScore = results => {
   return score
 }
 
+/** Validates a bowling game results array with frames and rolls and returns the argument if valid
+ *  otherwise throws an error
+ * @param {array} results - array of frame arrays
+ * @returns {array} results
+ */
 export const validateResults = results => {
   if (!_.every(results, r => _.isArray(r))) {
     throw new Error('Result frames must be arrays.')
@@ -29,6 +38,11 @@ export const validateResults = results => {
   } else return results
 }
 
+/** Returns the frame passed as argument if the frame qualifies as being a frame in a bowling game
+ *  otherwise throws an error
+ * @param {array} frame - current frame
+ * @returns {array} frame
+ */
 export const validateFrame = frame => {
   if (!_.every(frame, el => _.isNumber(el))) {
     throw new Error('Frame must only consist of numbers')
@@ -39,39 +53,71 @@ export const validateFrame = frame => {
   } else return frame
 }
 
+/** Returns the frame passed as argument if the frame qualifies as being on of the first nine frames in the game
+ *  otherwise throws an error
+ * @param {array} frame - current frame
+ * @returns {array} frame
+ */
 export const validateAsFirstFrame = frame => {
   if (frame.length > 2) throw new Error('Frame cannot have more than 2 rolls.')
   else return frame
 }
 
+/** Returns the frame passed as argument if the frame qualifies as being the last frame in the game
+ *  otherwise throws an error
+ * @param {array} frame - current frame
+ * @param {function} spareValidator - handler to determine if a frame is a spare
+ * @param {function} strikeValidator - handler to determine if a frame is a strike
+ * @returns {array} frame
+ */
 export const validateAsLastFrame = (frame, spareValidator = isSpare, strikeValidator = isStrike) => {
   if (frame.length === 3) {
     if (spareValidator(frame)) return frame
     else throw new Error('Last frame needs a spare in order to qualify for three rolls.')
   } else if (strikeValidator(frame) && frame.length === 1) {
     throw new Error('Last frame must have two rolls if first roll was a strike.')
-  }
+  } else return frame
 }
 
+/** Determines whether the frame is a spare
+ * @param {array} frame - current frame
+ * @returns {boolean}
+ */
 export const isSpare = frame => {
   if (frame[0] === 10) return false
   else if (frame[0] + frame[1] === 10) return true
   else return false
 }
 
+/** Determines whether the frame is a strike
+ * @param {array} frame - current frame
+ * @returns {boolean}
+ */
 export const isStrike = frame => {
   if (frame[0] === 10) return true
   else return false
 }
 
+/** Returns the score of the current frame
+ * @param {array} frame - current frame
+ * @returns {number}
+ */
 export const scoreForFrame = frame => {
   return (frame[0] + frame[1])
 }
 
+/** Returns the bonus if a spare happened in the current frame
+ * @param {array} nextFrame - the frame after the current frame
+ * @returns {number}
+ */
 export const bonusForSpare = nextFrame => {
   return nextFrame[0]
 }
 
+/** Returns the bonus if a strike happened in the current frame
+ * @param {array} nextFrame - the frame after the current frame
+ * @returns {number}
+ */
 export const bonusForStrike = nextFrame => {
   return nextFrame[0] + nextFrame[1]
 }
